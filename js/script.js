9cdac59c81ef0fd5b6a6b280fb00174c706c2d67
Penna92 <giacomopenna92@gmail.com>
2022-03-24 14:46:31 +0100

    script.js : fixed an exception that allowed to enter a negative value to age and ticket price.

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@ let distance = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 console.log("Distanza del percorso: " + 
 distance + " km");
 
+let age;
+
 if(isNaN(distance) || distance<0){
     alert('Valore non valido, puoi inserire solo 0 o numeri positivi: ricarica la pagina e riprova');
-} else if(!isNaN(distance)){document.getElementById('distanza').innerHTML += "Distanza del percorso: " + distance + " km";
+} else if(!isNaN(distance) && age>=0){document.getElementById('distanza').innerHTML += "Distanza del percorso: " + distance + " km";
 }
 
-let age = parseInt(prompt('Quanti anni hai?'));
+age = parseInt(prompt('Quanti anni hai?'));
 console.log("La tua età è: " + 
 age + " anni");
 
 if(isNaN(age) || age<0){
     alert('Valore non valido, puoi inserire solo 0 o numeri positivi: ricarica la pagina e riprova');
-} else if(!isNaN(age)){document.getElementById('age').innerHTML += "La tua età è: " + age + " anni";
+} else if(!isNaN(age) && distance>=0){document.getElementById('age').innerHTML += "La tua età è: " + age + " anni";
 }
 
 let price = distance * 0.21;
@@ -25,11 +27,11 @@ console.log("Prezzo minorenni: " + (Math.round(sale20 * 100) / 100).toFixed(2) +
 let sale40 = price / 100 * 60;
 console.log("Prezzo over 65: " + (Math.round(sale40 * 100) / 100).toFixed(2) + " €");
 
-if(age>=0 && age<18){
+if(age>=0 && age<18 && distance>=0){
     document.getElementById('finalprice').innerHTML += "Il prezzo del biglietto è: " + (Math.round(sale20 * 100) / 100).toFixed(2) + " €" ;
-} else if(age>=65){
+} else if(age>=65 && distance>=0){
     document.getElementById('finalprice').innerHTML += "Il prezzo del biglietto è: " + (Math.round(sale40 * 100) / 100).toFixed(2) + " €" ;
-} else if(isNaN(age) || age<0 || isNaN(distance)){
+} else if(isNaN(age) || age<0 || isNaN(distance) || distance<0){
     alert('Non è stato possibile calcolare il prezzo del biglietto: ripeti la procedura da capo inserendo dei valori validi');
 } else{
     document.getElementById('finalprice').innerHTML += "Il prezzo del biglietto è: " + (Math.round(price * 100) / 100).toFixed(2) + " €" ;   
