9cc3536ec31085ca17ea84ebadca2e43c56100c4
Penna92 <giacomopenna92@gmail.com>
2022-03-24 15:16:01 +0100

    script.js: modificato l'ordine degli operatori condizionali per fare in modo che i messaggi compariranno su schermo SOLO se tutti i valori inseriti dall'utente saranno validi.

--- js/script.js
+++ js/script.js
@@ -2,21 +2,17 @@ let distance = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 console.log("Distanza del percorso: " + 
 distance + " km");
 
-let age;
-
 if(isNaN(distance) || distance<0){
     alert('Valore non valido, puoi inserire solo 0 o numeri positivi: ricarica la pagina e riprova');
-} else if(!isNaN(distance) && age>=0){document.getElementById('distanza').innerHTML += "Distanza del percorso: " + distance + " km";
-}
+} 
 
-age = parseInt(prompt('Quanti anni hai?'));
+let age = parseInt(prompt('Quanti anni hai?'));
 console.log("La tua età è: " + 
 age + " anni");
 
 if(isNaN(age) || age<0){
     alert('Valore non valido, puoi inserire solo 0 o numeri positivi: ricarica la pagina e riprova');
-} else if(!isNaN(age) && distance>=0){document.getElementById('age').innerHTML += "La tua età è: " + age + " anni";
-}
+} 
 
 let price = distance * 0.21;
 console.log("Prezzo intero: " + (Math.round(price * 100) / 100).toFixed(2) + " €");
@@ -26,6 +22,12 @@ console.log("Prezzo minorenni: " + (Math.round(sale20 * 100) / 100).toFixed(2) +
 
 let sale40 = price / 100 * 60;
 console.log("Prezzo over 65: " + (Math.round(sale40 * 100) / 100).toFixed(2) + " €");
+
+if(!isNaN(distance) && age>=0 && distance>=0){document.getElementById('distanza').innerHTML += "Distanza del percorso: " + distance + " km";
+}
+
+if(!isNaN(age) && distance>=0 && age>=0){document.getElementById('age').innerHTML += "La tua età è: " + age + " anni";
+}
 
 if(age>=0 && age<18 && distance>=0){
     document.getElementById('finalprice').innerHTML += "Il prezzo del biglietto è: " + (Math.round(sale20 * 100) / 100).toFixed(2) + " €" ;
